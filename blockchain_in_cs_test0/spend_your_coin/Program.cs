09a7f69995e8a0c6b9a9dc59f45c14fb1d8fcd8d
jx01.mb <jx01.mb@jx01mb.local>
2018-10-06 01:27:15 -0400

    10-06-2018_00

--- blockchain_in_cs_test0/spend_your_coin/Program.cs
+++ blockchain_in_cs_test0/spend_your_coin/Program.cs
@@ -7,6 +7,16 @@ namespace spend_your_coin
     class Program
     {
         var network = Network.TestNet;
+
+        var privateKey = new Key();
+        var bitcoinPrivateKey = privateKey.GetWif(network);
+        var address = bitcoinPrivateKey.GetAddress();
+
+        Console.WriteLine(bitcoinPrivateKey);
+        Console.WriteLine(address);
+
+        var receivedCoins = transactionResponse.ReceivedCoins;
+        OutPoint outPointToSpend = null;
         
     }
 }
